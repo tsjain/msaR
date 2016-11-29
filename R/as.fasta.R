377b50b97c5636c5e4ef1897c81047be80436835
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-11-29 16:58:00 -0500

    add a method for ape's DNAbin

--- R/as.fasta.R
+++ R/as.fasta.R
@@ -1,6 +1,6 @@
 #' to_fasta functionality to convert objects to a fasta string
 #' 
-#' 
+#' @export
 setGeneric("as.fasta",function(obj) {standardGeneric("as.fasta")})
 #'
 #' @importFrom Biostrings DNAStringSet
@@ -37,15 +37,21 @@ setMethod("as.fasta",  signature(obj = "AAMultipleAlignment"), function(obj) {
 setMethod("as.fasta",  signature(obj = "RNAMultipleAlignment"), function(obj) {  
   as.fasta(DNAStringSet(obj))
 })
+#' @importFrom ape read.dna
 #' @importFrom Biostrings readDNAStringSet
 #' @importFrom Biostrings readAAStringSet
 #' @importFrom Biostrings readRNAStringSet
 setMethod("as.fasta",  signature(obj = "character"), function(obj) {  
-  try(sequences <- readDNAStringSet(obj))
+  try(sequences <- read.dna(obj, format = "fasta"))
   if (exists("sequences")) return(as.fasta(sequences))
   try(sequences <- readAAStringSet(obj))
   if (exists("sequences")) return(as.fasta(sequences))
   try(sequences <- readRNAStringSet(obj))
   if (exists("sequences")) return(as.fasta(sequences))
 })
-
+#'
+#'  @importFrom ape as.alignment
+setMethod("as.fasta",  signature(obj = "DNAbin"), function(obj) {  
+  aln <- as.alignment(obj)
+  paste0(">", aln$nam, "\n", aln$seq, collapse="\n")
+})
